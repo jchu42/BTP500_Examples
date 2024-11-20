cddd94087a5a1649c9951b68d11a998ee5ec1c10
joe <jchu42@myseneca.ca>
2024-11-20 13:47:37 -0500

    improved display function, ignore duplicate inserts, fixed search

--- Week_11/2_3.py
+++ Week_11/2_3.py
@@ -3,6 +3,7 @@
 recursively defined like my favourite BST and AVL bois.
 Still, interesting data structure!
 '''
+import random
 
 class Node:
     def __init__(self, key=None, is_leaf=False):
@@ -15,38 +16,51 @@ class Node:
         # Setting a flag for whether or not this is a leaf
         self.is_leaf = is_leaf
 
-    def display(self, node, indent, last):
+    def display(self, indent="", pos="─── "):
         '''We keep coming back to this function! So useful'''
-        print(indent, end="")
-        if last:
-            print("└── ", end="")
-            indent += "    "
+        if self.is_leaf:
+            print(indent, end="")
+            print(pos, end="")
+            print (self.keys)
         else:
-            print("├── ", end="")
-            indent += "|   "
-        print(node.keys)
-        for child in node.children:
-            self.display(child, indent, False)
+            for i in range(len(self.keys) + 1):
+                if i == 0:
+                    self.children[i].display(indent + "    ", pos="┌── ")
+                elif i == len(self.keys):
+                    self.children[i].display(indent + "    ", pos="└── ")
+                else:
+                    self.children[i].display(indent + "    ", pos="├── ")
+
+                if i < len(self.keys):
+                    print(indent, end="")
+                    print(pos, end="")
+                    print (self.keys[i])
     
     def insert_key(self, key):
         if self.is_leaf:
-            self.keys.append(key)
-            self.keys.sort()
-            if len(self.keys) == 3:
-                # make self the left node, return a tuple with the value to push up and
-                # the new sibling
-                to_return = (self.keys[1], Node(self.keys[2], is_leaf=True))
-                self.keys = [self.keys[0]]
-                return to_return
+            # values are only ever initially inserted in leaf nodes
+            if key not in self.keys:
+                self.keys.append(key)
+                self.keys.sort()
+                # if turns into a 4-node
+                if len(self.keys) == 3:
+                    # make self the left node, return a tuple with the value to push up and
+                    # the new sibling
+                    to_return = (self.keys[1], Node(self.keys[2], is_leaf=True))
+                    self.keys = [self.keys[0]]
+                    return to_return
             return None
         else:
             for i in range(len(self.keys) + 1):
+                # insert into correct child node
                 if i == len(self.keys) or key < self.keys[i]:
                     returned = self.children[i].insert_key(key)
+                    # if child was a 4-node
                     if returned is not None:
-                        # this should insert into correct positions?
+                        # insert key and child into correct positions
                         self.keys.insert(i, returned[0])
                         self.children.insert(i+1, returned[1])
+                        # if self has become a 4-node
                         if len(self.keys) == 3:
                             # make self the left node, return a tuple with the value to push
                             # up and the new sibling
@@ -70,14 +84,16 @@ class TwoThreeTree:
     def search_recursive(self, node, key):
         '''Helper function! Recursively searches the 2-3 tree'''
         # If the node is a leaf, just check if the key is here
-        if node.is_leaf:
-            return key in node.keys
+        if key in node.keys:
+            return True
+        if not node.is_leaf:
         # Otherwise, we need to traverse down the tree
-        for i, k in enumerate(node.keys):
-            if key < k:
-                return self.search_recursive(node.children[i], key)
-        # If the key is greater than all keys in this node, go to the last child
-        return self.search_recursive(node.children[-1], key)
+            for i, k in enumerate(node.keys):
+                if key < k:
+                    return self.search_recursive(node.children[i], key)
+            # If the key is greater than all keys in this node, go to the last child
+            return self.search_recursive(node.children[-1], key)
+        return False
     
     def insert_key(self, key):
         returned = self.root.insert_key(key)
@@ -90,7 +106,7 @@ class TwoThreeTree:
 
     def display(self):
         # Helper function to print the tree (for debugging purposes)
-        self.root.display(self.root, "", True)
+        self.root.display()
 
 
 if __name__ == "__main__":
@@ -100,7 +116,22 @@ if __name__ == "__main__":
     keys_to_insert = [10, 20, 5, 6, 15, 30, 25]
     for key in keys_to_insert:
         tree.insert_key(key)
-        tree.display()
+    tree.display()
 
+    print("Search 10:", tree.search(10))
     print("Search 15:", tree.search(15))
     print("Search 100:", tree.search(100))
+
+    tree = TwoThreeTree()
+    keys_to_insert = [i for i in range(100)]
+    random.shuffle(keys_to_insert)
+    for key in keys_to_insert:
+        tree.insert_key(key)
+    tree.display()
+    for i in range(100):
+        if not tree.search(i):
+            print ("error: couldn't find " + i)
+    if tree.search (-1):
+        print ("error: shouldn't have been able to find -1")
+    if tree.search (100):
+        print ("error: shouldn't have been able to find 100")
